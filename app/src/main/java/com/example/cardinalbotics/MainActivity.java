43e12a108df93d9bdb270c3b5dd00eb4265fa683
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-08-10 04:30:30 -0700

    It says I touched everything but it's literally crlf. Except I DID get the GET request to work!

--- app/src/main/java/com/example/cardinalbotics/MainActivity.java
+++ app/src/main/java/com/example/cardinalbotics/MainActivity.java
@@ -47,12 +47,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-//        I used this to test JSON parsing
-//        try {
-//            Request.requestCalendar();
-//        } catch(JSONException e) {
-//            e.printStackTrace();
-//        }
     }
 
     @Override
