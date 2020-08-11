709d6c8e9c2c93afdaa99da1d908b4c48f131bff
aychou <32346173+aychou@users.noreply.github.com>
2020-08-10 18:59:56 -0700

    training

--- app/src/main/java/com/example/cardinalbotics/ui/training/TrainingFragment.java
+++ app/src/main/java/com/example/cardinalbotics/ui/training/TrainingFragment.java
@@ -1,4 +1,29 @@
 package com.example.cardinalbotics.ui.training;
 
-public class TrainingFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.cardinalbotics.R;
+import com.example.cardinalbotics.ui.resources.ResourcesViewModel;
+
+public class TrainingFragment extends Fragment {
+
+    private TrainingViewModel resourcesViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        resourcesViewModel =
+                ViewModelProviders.of(this).get(ResourcesViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_resources, container, false);
+        return root;
+    }
 }
