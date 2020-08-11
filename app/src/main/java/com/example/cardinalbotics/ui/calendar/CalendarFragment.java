93dee5ac76ae1995ff1a4618d6b2e8269e706489
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-08-11 00:28:23 -0700

    Got urls for both external sources

--- app/src/main/java/com/example/cardinalbotics/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/cardinalbotics/ui/calendar/CalendarFragment.java
@@ -4,12 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CalendarView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.android.volley.Response;
+import com.example.cardinalbotics.AppSharedResources;
 import com.example.cardinalbotics.R;
+
+import org.json.JSONObject;
 
 public class CalendarFragment extends Fragment {
 
@@ -21,5 +28,17 @@ public class CalendarFragment extends Fragment {
                 ViewModelProviders.of(this).get(CalendarViewModel.class);
         View root = inflater.inflate(R.layout.fragment_calendar, container, false);
         return root;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        CalendarView calendar = ((CalendarView) getView().findViewById(R.id.calendarView));
+        AppSharedResources.getInstance(getActivity().getApplicationContext()).requestDataCalendar(new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                System.out.println("SUCCESS...?");
+                System.out.println(response.toString());
+            }
+        });
     }
 }
