9fc2afe5638e666eeeb1ab84fb38ef1696ba9d16
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-08-15 01:58:15 -0700

    Reformat more

--- app/src/main/java/com/example/cardinalbotics/MainActivity.java
+++ app/src/main/java/com/example/cardinalbotics/MainActivity.java
@@ -89,100 +89,100 @@ public class MainActivity extends AppCompatActivity {
 	}
 
 	public void link(View view) {
-		Intent browserIntent;
+		String url = "https://";
 		if (view.getId() == R.id.button1) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.team4159.org/"));
+			url = "https://www.team4159.org/";
 		} else if (view.getId() == R.id.button2) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://drive.google.com/drive/folders/1i3iPyM8DhkLzd8wtGz9jn305scfvdKW2?usp=sharing"));
+			url = "https://drive.google.com/drive/folders/1i3iPyM8DhkLzd8wtGz9jn305scfvdKW2?usp=sharing";
 		} else if (view.getId() == R.id.button3) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://docs.google.com/spreadsheets/d/1RdV0RmXQFTVhPmzd7iwHLWjBwX4bcGN4Cr1apzvLKGo/edit?usp=sharing"));
+			url = "https://docs.google.com/spreadsheets/d/1RdV0RmXQFTVhPmzd7iwHLWjBwX4bcGN4Cr1apzvLKGo/edit?usp=sharing";
 		} else if (view.getId() == R.id.button4) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://docs.google.com/spreadsheets/d/10tFI-9L5lEg6GgEVudibQn9Qtt8VgiWgW3QRCUARfD8/edit"));
+			url = "https://docs.google.com/spreadsheets/d/10tFI-9L5lEg6GgEVudibQn9Qtt8VgiWgW3QRCUARfD8/edit";
 		} else if (view.getId() == R.id.button5) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://calendar.google.com/calendar/embed?src=nicholas.do%40team4159.org&ctz=America%2FLos_Angeles"));
+			url = "https://calendar.google.com/calendar/embed?src=nicholas.do%40team4159.org&ctz=America%2FLos_Angeles";
 		} else if (view.getId() == R.id.button6) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://docs.google.com/spreadsheets/d/1po_TE36FA-I7J2Y-Biw5snWdfSm_Cx055KVi1c43G7Y/edit?usp=sharing"));
+			url = "https://docs.google.com/spreadsheets/d/1po_TE36FA-I7J2Y-Biw5snWdfSm_Cx055KVi1c43G7Y/edit?usp=sharing";
 		} else if (view.getId() == R.id.button7) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button8) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button9) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://drive.google.com/drive/folders/1rBSu0S9LPT8yfo2YVwzZgNt_JxE4767O?usp=sharing"));
+			url = "https://drive.google.com/drive/folders/1rBSu0S9LPT8yfo2YVwzZgNt_JxE4767O?usp=sharing";
 		} else if (view.getId() == R.id.button10) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://docs.google.com/document/d/135V9yQGAYNLl9aOCvyKJVLOLIh2yPcCEuZ69nNrumYY/edit?usp=sharing"));
+			url = "https://docs.google.com/document/d/135V9yQGAYNLl9aOCvyKJVLOLIh2yPcCEuZ69nNrumYY/edit?usp=sharing"));
 		} else {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://docs.google.com/document/d/17RjyV-YCr4_zKslnDachYI_9EAM11asWkexZfDYN4CM/edit?usp=sharing"));
+			url = "https://docs.google.com/document/d/17RjyV-YCr4_zKslnDachYI_9EAM11asWkexZfDYN4CM/edit?usp=sharing";
 		}
+		Intent browserIntent = new Intent(new Intent(Intent.ACTION_VIEW, Uri.parse(url));
 		startActivity(browserIntent);
 	}
 
 	public void training(View view) {
-		Intent browserIntent;
+		String url = "";
 		if (view.getId() == R.id.button12) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://docs.google.com/presentation/d/1-3JSEriTv5PFjdkyCE34i-5jmUD_zAhhj0TTWjjj3x4/edit?usp=sharing"));
+			url = "https://docs.google.com/presentation/d/1-3JSEriTv5PFjdkyCE34i-5jmUD_zAhhj0TTWjjj3x4/edit?usp=sharing";
 		} else if (view.getId() == R.id.button13) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
 		} else if (view.getId() == R.id.button14) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button15) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button16) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button17) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button18) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button19) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button20) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button21) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
-			startActivity(browserIntent);
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button22) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
-			startActivity(browserIntent);
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button23) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button24) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button25) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button26) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button27) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button28) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button29) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button30) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button31) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button32) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button33) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button34) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button35) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button36) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button37) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button38) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button39) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button40) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button41) {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
 		} else {
-			browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		}
+		Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
 		startActivity(browserIntent);
 	}
 }
