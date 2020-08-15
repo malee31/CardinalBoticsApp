bc63a4920210262fa9b2e323b21b7029c7f23ce6
aychou <32346173+aychou@users.noreply.github.com>
2020-08-15 02:55:03 -0700

    links changed

--- app/src/main/java/com/example/cardinalbotics/MainActivity.java
+++ app/src/main/java/com/example/cardinalbotics/MainActivity.java
@@ -128,61 +128,61 @@ public class MainActivity extends AppCompatActivity {
 		} else if (view.getId() == R.id.button15) {
 			url = "https://docs.google.com/presentation/d/16gn59qkjmUjYd--Mud4NDHDZpljU24xEVJ0jfXFNIyw/edit?usp=sharing";
 		} else if (view.getId() == R.id.button16) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "https://docs.google.com/presentation/d/1WDbFyScvhplqefYX4b-AZP-TlWCloticNAgK4LUTPDc/edit?usp=sharing";
 		} else if (view.getId() == R.id.button17) {
 			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button18) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "https://codingbat.com/python";
 		} else if (view.getId() == R.id.button19) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "https://www.learnpython.org/";
 		} else if (view.getId() == R.id.button20) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "https://www.tutorialspoint.com/python/index.htm";
 		} else if (view.getId() == R.id.button21) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "https://www.codecademy.com/learn/learn-python";
 		} else if (view.getId() == R.id.button22) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "https://robotpy.readthedocs.io/en/stable/";
 		} else if (view.getId() == R.id.button23) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "http://www.citruscircuits.org/uploads/6/9/3/4/6934550/robot_wiring.pdf";
 		} else if (view.getId() == R.id.button24) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "";
 		} else if (view.getId() == R.id.button25) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "https://www.codecademy.com/learn/learn-java";
 		} else if (view.getId() == R.id.button26) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "https://codingbat.com/java";
 		} else if (view.getId() == R.id.button27) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "https://github.com/Team4159";
 		} else if (view.getId() == R.id.button28) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "https://www.youtube.com/watch?v=uGtT8ojgSzg";
 		} else if (view.getId() == R.id.button29) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "https://drive.google.com/drive/folders/1gisd-FH-XU8wbz2M4Mr-SzQx0UQNwspj";
 		} else if (view.getId() == R.id.button30) {
 			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button31) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "https://docs.google.com/presentation/d/1VDdrfGJsfbc2MEezSSUdEgt_KqQ3IdYvN3mgrX8tmwI/edit?usp=sharing";
 		} else if (view.getId() == R.id.button32) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "https://www.youtube.com/playlist?list=PLOKjEBaXxWbnwVZOEO9c0qaKvn-9O79Va";
 		} else if (view.getId() == R.id.button33) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "https://docs.google.com/presentation/d/1RUE8cu9ajZRU7McBT-yi0a71mNaSPH6T2xEBwsBK2xM/edit#slide=id.p";
 		} else if (view.getId() == R.id.button34) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "https://www.youtube.com/playlist?list=PLdhKcDv50ts4sUT_8LhrrAsd6VUVgtTwd";
 		} else if (view.getId() == R.id.button35) {
 			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button36) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "https://www.youtube.com/playlist?list=PLdhKcDv50ts5ecZ4ML5bJVuMVUdPriRyL";
 		} else if (view.getId() == R.id.button37) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "https://docs.google.com/presentation/d/1P5eF17pxyB4FdQ4eowdGZJCe5NBzLqwVASZN1GCMN-M/edit?usp=sharing";
 		} else if (view.getId() == R.id.button38) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "https://docs.google.com/document/d/1s8yZl4swGWui5kfPxrPaXTZvgV3YQAYgpHOWLe_y96U/edit";
 		} else if (view.getId() == R.id.button39) {
 			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		} else if (view.getId() == R.id.button40) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "https://drive.google.com/drive/folders/0B1En7yzIhCm6UEZDMkdhdTdXYm8?usp=sharing";
 		} else if (view.getId() == R.id.button41) {
-			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
+			url = "https://www.youtube.com/playlist?list=PLdhKcDv50ts4elT5X0z1aaDkMYGTCw_cE";
 		} else {
 			url = "https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ";
 		}
-		Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCDc0DVwU0NN62J98qPxuurQ"));
+		Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
 		startActivity(browserIntent);
 	}
 }
