47ed5ce54d20f9aae450d510f40b5abc41e924c9
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-08-16 01:39:36 -0700

    Dummy data

--- app/src/main/java/com/example/cardinalbotics/ui/timelog/TimeLogFragment.java
+++ app/src/main/java/com/example/cardinalbotics/ui/timelog/TimeLogFragment.java
@@ -1,11 +1,9 @@
 package com.example.cardinalbotics.ui.timelog;
 
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,12 +11,11 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.android.volley.Response;
 import com.example.cardinalbotics.AppSharedResources;
 import com.example.cardinalbotics.R;
-import com.example.cardinalbotics.ui.login.LoginViewModel;
-import com.google.android.material.snackbar.Snackbar;
 
-import static com.example.cardinalbotics.AppSharedResources.running;
+import org.json.JSONObject;
 
 public class TimeLogFragment extends Fragment {
 	TextView textView;
@@ -32,19 +29,38 @@ public class TimeLogFragment extends Fragment {
 
 	@Override
 	public void onViewCreated(final View view, @Nullable Bundle savedInstanceState) {
-//		String startTime = AppSharedResources.getInstance(getActivity().getApplicationContext()).storeGet("com.example.cardinalbotics.timestart");
-//		long timeTnSeconds =SystemClock.elapsedRealtime()/1000;
-//		if(startTime.equals("No Entry")){
+		AppSharedResources shared = AppSharedResources.getInstance(getActivity().getApplicationContext());
+//		String startTime = shared.storeGet("com.example.cardinalbotics.timestart");
+//		long timeTnSeconds = SystemClock.elapsedRealtime()/1000;
+//		if(startTime.equals("No Entry")) {
 //			return;
-//		}else if(running==false){
+//		} else if(shared.running == false) {
 //			return;
-//		}else{
-//			long longStartTime=Long.parseLong(AppSharedResources.getInstance(getActivity().getApplicationContext()).storeGet("com.example.cardinalbotics.timestart"));
-//			long totalTimeIn=Math.subtractExact(timeTnSeconds, longStartTime);
+//		} else {
+//			long longStartTime = Long.parseLong(AppSharedResources.getInstance(getActivity().getApplicationContext()).storeGet("com.example.cardinalbotics.timestart"));
+//			long totalTimeIn = Math.subtractExact(timeTnSeconds, longStartTime);
 //		}
 		int hour=5, minute=7;
 		textView = view.findViewById(R.id.time);
 		textView.setText("Did not Sign in");
 		textView.setText(hour+" : "+ minute);
+
+		shared.requestDataCalendar(new Response.Listener<JSONObject>() {
+			@Override
+			public void onResponse(JSONObject response) {
+				try {
+//					response = new JSONObject("{\"log\": [[\"timeIn\":\"12/9/2020T15:30:30\", \"timeOut\":\"12/9/2020T17:45:00\", \"info\": \"Did stuffs\"],[\"timeIn\":\"12/19/2020T15:30:30\", \"timeOut\":\"12/19/2020T17:45:00\", \"info\": \"Did more stuffs\"],[\"timeIn\":\"12/21/2020T15:30:30\", \"" + "timeOut\":\"12/21/2020T18:00:00\", \"info\": \"Finished stuffs\"]]}");
+					int month = 7, day = 10, year = 2020, hour=15, minute=7, second=20;
+					String didwhat = "Stuff done alot";
+					for(int log = 0; log < 10; log++) {
+						textView = view.findViewById(R.id.time);
+						textView.setText("Did not Sign in");
+						textView.setText(hour+" : "+ minute);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 }
